614bd2515dabcd597f178ec9441f4880dcbf9c70
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2023-07-17 21:40:14 +0900

    First version

---
+++ index.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const twintentUrl = 'https://twitter.com/intent/tweet';
+
+async function
+main()
+{
+	const tabs = await chrome.tabs.query({ active: true });
+	const tab = tabs[0];
+
+	const url = new URL(twintentUrl);
+	const params = url.searchParams;
+	params.set('text', tab.title);
+	params.set('url', tab.url);
+
+	location.href = url.toString();
+}
+
+main();
