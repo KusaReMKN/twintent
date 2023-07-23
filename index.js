28e4d73c52b5b0c69b3765fde298ed78e04c2f9c
KusaReMKN <48670724+KusaReMKN@users.noreply.github.com>
2023-07-24 03:32:58 +0900

    Change: 複数のプラットフォームに対応

--- index.js
+++ index.js
@@ -1,22 +1,119 @@
 'use strict';
 
-const twintentUrl = 'https://twitter.com/intent/tweet';
+import * as Settings from './settings.js';
 
+/**
+ * 要素を初期化する。
+ * 読み込み完了時のイベントハンドラ。
+ */
 async function
-main()
+initialize(_)
 {
-	const tabs = await chrome.tabs.query({
-		active: true,
-		currentWindow: true,
-	});
-	const tab = tabs[0];
+    await Settings.migrate();
 
-	const url = new URL(twintentUrl);
-	const params = url.searchParams;
-	params.set('text', tab.title);
-	params.set('url', tab.url);
+    const manifest = chrome.runtime.getManifest();
+    window.versionStr.textContent = `(v${manifest.version})`
 
-	location.href = url.toString();
+    const items = await Settings.getAllItems();
+    Object.keys(items).forEach(key => {
+        const item = items[key];
+        const option = document.createElement('option');
+        option.textContent = item.name;
+        option.setAttribute('value', key);
+        window.shareVia.appendChild(option.cloneNode(true));
+        window.itemList.appendChild(option.cloneNode(true));
+    });
 }
+window.addEventListener('DOMContentLoaded', initialize);
 
-main();
+/**
+ * 共有する。
+ * 共有フォームのイベントハンドラ。
+ */
+async function
+share(e)
+{
+    e.preventDefault();
+    window.btnShare.disabled = true;
+    window.btnShare.value = '😁 待たれよ';
+
+    const tabs = await chrome.tabs.query({
+        active: true,
+        currentWindow: true,
+    });
+    const tab = tabs[0];
+
+    const item = await Settings.getItem(window.shareVia.value);
+    const url = new URL(item.url);
+    if (item.urlKey === null) {
+        url.searchParams.set(item.textKey, tab.title + ' ' + tab.url);
+    } else {
+        url.searchParams.set(item.textKey, tab.title);
+        url.searchParams.set(item.urlKey, tab.url);
+    }
+    window.location.href = url.toString();
+
+    return false;
+}
+window.formShare.addEventListener('submit', share);
+
+/**
+ * 設定項目を更新する。
+ * itemList のイベントハンドラ。
+ */
+async function
+changeList(e)
+{
+    const item = await Settings.getItem(e.target.value)
+        || { name: '', url: '', textKey: '', urlKey: '' };
+
+    window.itemName.value = item.name;
+    window.itemUrl.value = item.url;
+    window.itemTextKey.value = item.textKey;
+    window.itemUrlKey.value = item.urlKey || '';
+    window.itemUrlAsText.checked = item.urlKey === null;
+    window.itemUrlKey.disabled = window.itemUrlAsText.checked;
+}
+window.itemList.addEventListener('change', changeList);
+
+/**
+ * URL 用 key の有効無効を切り替える。
+ * itemUrlAsText のイベントハンドラ。
+ */
+function
+switchUrlKey(e)
+{
+    window.itemUrlKey.disabled = e.target.checked;
+}
+window.itemUrlAsText.addEventListener('change', switchUrlKey);
+
+/**
+ * 要素を編集する。
+ *
+ */
+async function
+modifyItem(e)
+{
+    e.preventDefault();
+    e.submitter.disabled = true;
+    e.submitter.value = '😁 待たれよ';
+
+    const item = await Settings.getItem(window.itemList.value) || {};
+    if (e.submitter.name === 'remove') {
+        if (window.confirm(`🛒 正気か？ ${item.name} は消えます‼️`))
+            await Settings.removeItem(window.itemList.value);
+    } else {
+        item.name = window.itemName.value;
+        item.url = window.itemUrl.value;
+        item.textKey = window.itemTextKey.value;
+        item.urlKey = window.itemUrlAsText.checked ? null : window.itemUrlKey.value;
+        if (window.itemList.value === 'newItem')
+            await Settings.addItem(item);
+        else
+            await Settings.setItem(window.itemList.value, item);
+    }
+
+    window.location.reload();
+    return false;
+}
+window.modItem.addEventListener('submit', modifyItem);
---
+++ settings.js
@@ -0,0 +1,109 @@
+'use strict';
+
+/**
+ * 新しい設定項目を追加する。
+ *
+ * @param { { name: string, url: string, textKey: string, urlKey: string | null } } item
+ * 追加する項目
+ */
+export async function
+addItem(item)
+{
+    await setItem(window.crypto.randomUUID(), item);
+}
+
+/**
+ * 全てを消し去って初期状態に戻す。
+ * chrome.storage に保存されている情報は全て削除される。
+ *
+ * @param { string } yes 正気なら 'yes'
+ */
+export async function
+clear(yes)
+{
+    if (yes === 'yes')
+        await chrome.storage.local.clear();
+}
+
+/**
+ * 全ての設定項目の一覧を返す。
+ *
+ * @returns { Promise<{ name: string, url: string, textKey: string, urlKey: string | null }[]> }
+ */
+export async function
+getAllItems()
+{
+    const settings = await chrome.storage.local.get();
+    return settings.items;
+}
+
+/**
+ * 指定された設定項目を返す。
+ *
+ * @param { string } id 要求する項目の識別子
+ * @returns { Promise<{ name: string, url: string, textKey: string, urlKey: string | null } | undefined> }
+ */
+export async function
+getItem(id)
+{
+    const items = await getAllItems();
+    return items[id];
+}
+
+/**
+ * 保存済の設定を現在のバージョンに移行する。
+ * chrome.storage に保存されている情報は更新される。
+ */
+export async function
+migrate()
+{
+    const manifest = chrome.runtime.getManifest();
+    const settings = await chrome.storage.local.get();
+
+    while (settings.version !== manifest.version)
+        switch (settings.version) {
+            case undefined:	/* first boot */
+                const sampleItem = {
+                    name: 'Twitter',
+                    url: 'https://twitter.com/intent/tweet',
+                    textKey: 'text',
+                    urlKey: 'url',
+                };
+                settings.items = {};
+                settings.items[window.crypto.randomUUID()] = sampleItem;
+                settings.version = manifest.version;
+                window.alert('🌪️ twintent をインストールしてくれてありがとう⁉️');
+                break;
+            default:	/* no need to migrate */
+                settings.version = manifest.version;
+        }
+    await chrome.storage.local.set(settings);
+}
+
+/**
+ * 指定した設定項目を削除する。
+ *
+ * @param { string } id 削除する項目の識別子
+ */
+export async function
+removeItem(id)
+{
+    const settings = await chrome.storage.local.get();
+    delete settings.items[id];
+    await chrome.storage.local.set(settings);
+}
+
+/**
+ * 指定した設定項目を更新する。
+ *
+ * @param { string } id 更新する項目の識別子
+ * @param { { name: string, url: string, textKey: string, urlKey: string | null } } item
+ * 更新するする項目
+ */
+export async function
+setItem(id, item)
+{
+    const settings = await chrome.storage.local.get();
+    settings.items[id] = item;
+    await chrome.storage.local.set(settings);
+}
